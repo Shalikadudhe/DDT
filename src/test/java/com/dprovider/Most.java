920147f61563f681af89b102297c0d36209e0198
Shalikadudhe <110039706+Shalikadudhe@users.noreply.github.com>
2024-03-01 20:22:00 +0530

    Update Most.java
    Most files has some changes

--- src/test/java/com/dprovider/Most.java
+++ src/test/java/com/dprovider/Most.java
@@ -4,7 +4,10 @@ public class Most {
 	
 	//adding class 
 	public static void main(String[] args) {
-		System.out.println("hello ....");
+		System.out.println("Hello forks.....");
+
+   
+		
 	}
 
 }
